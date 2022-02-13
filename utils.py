8ea979072531cffa74cf26b0b53746abac34ff30
Abdelaziz Sbaai <abdelaziz.sbaai@etu.univ-lyon1.fr>
2022-02-13 20:23:12 +0100

    analyse

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def isCyclique(speeds=[]):
     if(len(speeds) > 1):
         first = speeds[0]
@@ -8,17 +11,9 @@ def isCyclique(speeds=[]):
         if index_cycle > 1:
             cycle = speeds[0:index_cycle]
             if 0 in cycle:
-                return {
-                    'cyclique': False
-                }
-            return {
-                'cyclique': True,
-                'cycle': cycle,
-                'fenetre': len(cycle),
-            }
-    return {
-        'cyclique': False
-    }
+                return ('non cyclique')
+            return ('cyclique', cycle, len(cycle))
+    return ('non cyclique')
 
 
 def isRegulier(speeds=[]):
@@ -26,16 +21,9 @@ def isRegulier(speeds=[]):
         first = speeds[0]
         for speed in speeds[1:2]:
             if speed == 0 or speed != first:
-                return {
-                    'regulier': False
-                }
-        return {
-            'regulier': True,
-            'vitesse': first,
-        }
-    return {
-        'regulier': False
-    }
+                return ('non regulier')
+        return ('regulier', first)
+    return ('non regulier')
 
 
 def isFatiguee(speeds=[]):
@@ -43,29 +31,45 @@ def isFatiguee(speeds=[]):
         try:
             first_zero_index = speeds.index(0)
         except:
-            return {
-                'fatigue': False
-            }
+            return ('non fatigue')
         try:
             second_zero_index = first_zero_index + \
                 speeds[first_zero_index + 1:].index(0) + 1
         except:
-            return {
-                'fatigue': False
-            }
+            return ('non fatigue')
         cycle = speeds[first_zero_index:second_zero_index + 1]
         max_speed = max(cycle)
         rythme = max_speed - cycle[cycle.index(max_speed) + 1]
         if rythme == 0:
-            return {
-                'fatigue': False
-            }
-        return {
-            'fatigue': True,
-            'vitesse_initiale': max_speed,
-            'rythme': rythme,
-        }
+            ('non fatigue')
+        return ('fatigue', max_speed, rythme)
 
-    return {
-        'fatigue': False
-    }
+    return ('non fatigue')
+
+def isSameCycle(cycle1, cycle2):
+    fc1 = cycle1[0]
+    lc2 = cycle2.index(fc1)
+    tc2 = cycle2[lc2:] + cycle2[:lc2]
+    return cycle1 == tc2
+
+
+def test(speeds):
+    temp = 0.0
+    quali = 0.0
+    regulier = isRegulier(speeds)
+    cyclique = isCyclique(speeds)
+    fatigue = isFatiguee(speeds)
+
+    res = (temp, quali, {'isGood': False})
+
+    if regulier['regulier']:
+        return (temp, quali, {'result': regulier, 'isGood': True})
+    if cyclique['cyclique']:
+        return (temp, quali, {'result': cyclique, 'isGood': True})
+    if fatigue['fatigue']:
+        return (temp, quali, {'result': fatigue, 'isGood': True})
+    return (temp, quali, {'isGood': False})
+
+
+if __name__ == "__main__":
+    print(test([3, 2, 1, 0, 1, 2, 3, 2, 1, 0]))
